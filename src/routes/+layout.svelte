a4f51bded1b8146696f762c721726f8c730999a6
YammD123 <siamqueen52@gmail.com>
2025-10-25 02:29:27 +0700

    feat: menambahkan provider dari tanstack query

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+	import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools'
 
 	let { children } = $props();
+	const queryClient = new QueryClient();
 </script>
+
+
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<QueryClientProvider client={queryClient}>
+	{@render children?.()}
+	<SvelteQueryDevtools initialIsOpen={false} position='bottom' />
+</QueryClientProvider>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,17 +1,17 @@
-import { authService } from "../services/authService";
-import type { LayoutLoad } from "./$types";
+import { authService } from '../services/authService';
+import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = async () => {
-  try {
-    const authSession = await authService.startSession();
-    console.log("Layout load auth session:", authSession);
-    return {
-      authSession
-    };
-  } catch (err) {
-    console.error("❌ Gagal mengambil session:", err);
-    return {
-      authSession: null
-    };
-  }
+	try {
+		const authSession = await authService.startSession();
+		console.log('Layout load auth session:', authSession);
+		return {
+			authSession
+		};
+	} catch (err) {
+		console.error('❌ Gagal mengambil session:', err);
+		return {
+			authSession: null
+		};
+	}
 };
